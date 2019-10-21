e156ed61b12c26fdc07f5a368d2bdc275bf8d73d
HowieWang <howiewh@163.com>
2019-10-21 16:58:48 +0800

    fix encrypt bug in android p bug

--- core/src/main/java/cn/authing/core/utils/AuthingUtils.kt
+++ core/src/main/java/cn/authing/core/utils/AuthingUtils.kt
@@ -1,5 +1,6 @@
 package cn.authing.core.utils
 
+import android.os.Build
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.bouncycastle.util.encoders.Base64
 import java.security.KeyFactory
@@ -10,6 +11,7 @@ import javax.crypto.Cipher
 internal object AuthingUtils {
     var URL_OAUTH = "https://oauth.authing.cn/graphql"
     var URL_USER = "https://users.authing.cn/graphql"
+    private const val ANDROID_VERSION_CODE_P = 28
 
     private val PUBLICA_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC4xKeUgQ+Aoz7TLfAfs9+paePb" +
             "5KIofVthEopwrXFkp8OCeocaTHt9ICjTT2QeJh6cZaDaArfZ873GPUn00eOIZ7Ae" +
@@ -24,7 +26,8 @@ internal object AuthingUtils {
             // 获取公钥
             val keyBytes = Base64.decode(PUBLICA_KEY)
             val keySpec = X509EncodedKeySpec(keyBytes)
-            val keyFactory = KeyFactory.getInstance("RSA", provider)
+            // 兼容 Android P https://android-developers.googleblog.com/2018/03/cryptography-changes-in-android-p.html
+            val keyFactory = if (Build.VERSION.SDK_INT >= ANDROID_VERSION_CODE_P) KeyFactory.getInstance("RSA") else KeyFactory.getInstance("RSA", provider)
             val publicKey = keyFactory.generatePublic(keySpec)
             val cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding", provider)
             cipher.init(Cipher.ENCRYPT_MODE, publicKey)
