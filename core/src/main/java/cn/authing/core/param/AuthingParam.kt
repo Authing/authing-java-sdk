916cf707a53ab674b7a71ee9c191c7222e487e3f
stillfighting <2522910086@qq.com>
2020-03-11 23:58:26 +0800

    v2

--- core/src/main/java/cn/authing/core/param/AuthingParam.kt
+++ core/src/main/java/cn/authing/core/param/AuthingParam.kt
@@ -1,12 +1,12 @@
 package cn.authing.core.param
 
 /**
- * 网络请求参数的基类
- * 要求：
- * 1）其子类都需要使用 Builder 设计模式
- * 2）子类只允许有一个以 Builder 为参数的非 public 的构造方法
- * 3）Builder 的构造方法只允许有必须参数
- * 4）所有的参数处理均在 Builder#build 方法中，例如加密，填充 clientId
+ * Base class of network request parameters
+ * Requirement：
+ * 1）All its son need to use the Builder Design Pattern
+ * 2）Only one non public constructor with builder as parameter is allowed for son
+ * 3）Only required parameters are allowed for builder's construction method
+ * 4）All parameter processing is in the Bilder#build method, such as encryption and filling in ClientID
  */
 internal open class AuthingParam<T>(
         protected val query: String
