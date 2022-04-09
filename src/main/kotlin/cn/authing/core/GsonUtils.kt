e03a66a9bb55f19fdfdce2c5dfca1a41216931a1
weichenglong <1527649402@qq.com>
2022-04-09 19:39:36 +0800

    convert to kotlin

--- src/main/kotlin/cn/authing/core/GsonUtils.kt
+++ src/main/kotlin/cn/authing/core/GsonUtils.kt
@@ -1,20 +1,16 @@
-package cn.authing.core;
+package cn.authing.core
 
-import com.google.gson.TypeAdapterFactory;
-import com.google.gson.internal.bind.ObjectTypeAdapter;
+import com.google.gson.TypeAdapterFactory
 
-import java.util.List;
-
-public class GsonUtils {
-
-    public static Integer getIndex(List<TypeAdapterFactory> list){
-        for (int i = 0; i < list.size(); i++) {
-            TypeAdapterFactory factory = list.get(i);
-            Class<? extends TypeAdapterFactory> aClass = factory.getClass();
-            if (aClass.getName().contains("ObjectTypeAdapter")){
-                return i;
+object GsonUtils {
+    fun getIndex(list: List<TypeAdapterFactory>): Int {
+        for (i in list.indices) {
+            val factory = list[i]
+            val aClass: Class<out TypeAdapterFactory> = factory.javaClass
+            if (aClass.name.contains("ObjectTypeAdapter")) {
+                return i
             }
         }
-        return null;
+        return 1;
     }
 }
