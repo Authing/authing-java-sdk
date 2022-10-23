b25b32d92466f62ad8ae6e9d653bb7fd9d0292bd
廖长江 <cj@authing.cn>
2022-10-23 21:50:09 +0800

    feat: update version

--- src/main/java/cn/authing/sdk/java/enums/SignatureEnum.java
+++ src/main/java/cn/authing/sdk/java/enums/SignatureEnum.java
@@ -1,7 +1,6 @@
 package cn.authing.sdk.java.enums;
 
 public enum SignatureEnum {
-    X_AUTHING_SDK_CLIENT("Node.js(v14.18.0), authing-java-sdk: 0.0.19"),
     X_AUTHING_SIGNATURE_METHOD("HMAC-SHA1"),
     X_AUTHING_SIGNATURE_VERSION("1.0");
 
