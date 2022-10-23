172fe7d79907078d55553e7d3cefd803473d1d82
李锡勇 <lixiyong@authing.cn>
2022-10-23 18:11:44 +0800

    feat: 替换明文

--- src/test/java/test/management/GetSecuritySettingsTest.java
+++ src/test/java/test/management/GetSecuritySettingsTest.java
@@ -7,16 +7,16 @@ import cn.authing.sdk.java.util.JsonUtils;
 
 public class GetSecuritySettingsTest {
     // 需要替换成你的 Authing Access Key ID
-    private static final String ACCESS_KEY_ID = "634cfaa10cdd0b52923d0b35";
+    private static final String ACCESS_KEY_ID = "AUTHING_ACCESS_KEY_ID";
     // 需要替换成你的 Authing Access Key Secret
-    private static final String ACCESS_KEY_SECRET = "7b1554be16e9f4b8952a80618f9267f4";
+    private static final String ACCESS_KEY_SECRET = "AUTHING_ACCESS_KEY_SECRET";
 
     public static void main(String[] args) throws Throwable {
         ManagementClientOptions clientOptions = new ManagementClientOptions();
         clientOptions.setAccessKeyId(ACCESS_KEY_ID);
         clientOptions.setAccessKeySecret(ACCESS_KEY_SECRET);
         // 如果是私有化部署的客户，需要设置 Authing 服务域名
-         clientOptions.setHost("https://kuluqi.test2.authing-inc.co");
+        // clientOptions.setHost("https://api.your-authing-service.com");
 
         ManagementClient client = new ManagementClient(clientOptions);
 
