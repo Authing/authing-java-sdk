f7cd4d027e8c050167bfe07dae9b32bc7c2c3a40
yujiale <yu18371919563@163.com>
2022-12-13 10:04:34 +0800

    feat:协作管理员 ak/sk 方法生成

---
+++ src/main/java/cn/authing/sdk/java/dto/ListAccessKeyRespDto.java
@@ -0,0 +1,83 @@
+package cn.authing.sdk.java.dto;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public class ListAccessKeyRespDto {
+    /**
+     * 用户所拥有的 accessKeyId
+     */
+    @JsonProperty("accessKeyId")
+    private String accessKeyId;
+    /**
+     * 用户 ID
+     */
+    @JsonProperty("userId")
+    private String userId;
+    /**
+     * 创建时间
+     */
+    @JsonProperty("createdAt")
+    private String createdAt;
+    /**
+     * accessKeyId 状态
+     */
+    @JsonProperty("status")
+    private String status;
+    /**
+     * 最后使用时间
+     */
+    @JsonProperty("lastDate")
+    private String lastDate;
+    /**
+     * accessKeyId 所在用户池
+     */
+    @JsonProperty("userPoolId")
+    private String userPoolId;
+
+    public String getAccessKeyId() {
+        return accessKeyId;
+    }
+    public void setAccessKeyId(String accessKeyId) {
+        this.accessKeyId = accessKeyId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getLastDate() {
+        return lastDate;
+    }
+    public void setLastDate(String lastDate) {
+        this.lastDate = lastDate;
+    }
+
+    public String getUserPoolId() {
+        return userPoolId;
+    }
+    public void setUserPoolId(String userPoolId) {
+        this.userPoolId = userPoolId;
+    }
+
+
+
+}
